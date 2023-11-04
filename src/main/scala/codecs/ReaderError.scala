3164ea64286d07f259257d831b18f5609aa48236
Yaroslav <y.kivaev@gmail.com>
2023-11-04 22:47:51 +0800

    fix codestyle

--- src/main/scala/codecs/ReaderError.scala
+++ src/main/scala/codecs/ReaderError.scala
@@ -3,5 +3,4 @@ package codecs
 sealed abstract class ReaderError
 case class WrongType(field: String, message: String = "Wrong field type") extends ReaderError
 case class AbsentField(field: String, message: String = "Absent field") extends ReaderError
-case class ReaderErrors(list: List[ReaderError], field: String, message: String = "Several errors")
-  extends ReaderError
+case class ReaderErrors(list: List[ReaderError], field: String, message: String = "Several errors") extends ReaderError
